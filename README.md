4fa000ed9111593b86e263828fad004e8932d6a2
abbach_n <nabil.abbach@epitech.eu>
2018-01-13 23:56:47 +0100

    First

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 On utilise SQLite PCL comme lib
 
 pour l'instant ce ne fait que afficher certain truc en base de donnée 
+
+
+lien utile : https://melinyel.net/topic/1700-tutoriel-commencer-%C3%A0-utiliser-sqlite-avec-c/
+https://www.codeproject.com/Articles/22165/Using-SQLite-in-your-C-Application
+
+logicile de converion ^^ : https://dbconvert.com/mssql/sqlite/
---
+++ hololens_v10/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SQLitePCL;
+using Windows.Storage; //Files, Folders 
+
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace hololens_v10
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public SQLiteConnection dbConnection = new SQLiteConnection("virtual_deck.db");
+
+
+        private void btnFill_List_Click(object sender, RoutedEventArgs e)
+        {
+            var items = new List<String>();
+
+            string sSQL = @"SELECT [name],[description] FROM games";
+            ISQLiteStatement dbState = dbConnection.Prepare(sSQL);
+            while (dbState.Step() == SQLiteResult.ROW)
+            {
+                string sFoldername = dbState["name"] as string;
+                string sPath = dbState["description"] as string;
+
+                //< correction > 
+                sFoldername = sFoldername.Replace("''", "'");
+                sPath = sPath.Replace("''", "'");
+                //</ correction > 
+
+                items.Add(sFoldername + "; " + sPath);
+            }
+            ctlList.ItemsSource = items;
+        }
+
+        private async void btnOpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder local_CacheFolder = ApplicationData.Current.LocalCacheFolder;
+            await Windows.System.Launcher.LaunchFolderAsync(local_CacheFolder);
+            //</ Open App Cache Folder > 
+
+            // Debug.WriteLine();
+
+        }
+    }
+}
